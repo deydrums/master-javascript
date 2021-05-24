40f239fb6f7f7e703490836195f7362974d0a2d1
deydrums <dagarcia100@gmail.com>
2021-05-24 16:33:30 -0600

    Variables y comentarios

--- hola-mundo/js/01-hola-mundo.js
+++ hola-mundo/js/01-hola-mundo.js
@@ -1,5 +1,9 @@
+
 alert("Hola Mundo con JS");
 alert("Bienvenido al curso");
-document.write("Hola mundo desde fichero externo");
+//document.write("Hola mundo desde fichero externo");
+
 console.log("Muestra esto en la consola");
 console.log(88+4);
+
+
---
+++ hola-mundo/js/02-variables.js
@@ -0,0 +1,16 @@
+//VARIABLES
+
+//Una variable es un contenedor de información
+
+var pais = "Guatemala";
+var continente = "America";
+var antiguedad = 2021;
+var pais_y_continente = pais + ' ' + continente;
+
+pais = "Estados Unidos";
+continente = "Norte America";
+
+console.log(pais,continente,antiguedad);
+console.log(pais_y_continente);
+alert(pais_y_continente);
+console.log(antiguedad);
