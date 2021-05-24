d0170f30ce53490dbfefc9fca85d75698a3060be
deydrums <dagarcia100@gmail.com>
2021-05-24 16:49:40 -0600

    Constantes

---
+++ hola-mundo/js/04-constantes.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Constantes
+//Como una variable, pero su valor no puede cambiar
+
+var web = "facebook.com";
+const ip = "127.0.0.1";
+
+web = "google.com";
+console.log(web, ip);
