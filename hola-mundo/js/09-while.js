e1e607c935ba8e15f6ba336d4e63b9dd3efbcb25
deydrums <dagarcia100@gmail.com>
2021-05-24 21:34:00 -0600

    Do While

--- hola-mundo/js/09-while.js
+++ hola-mundo/js/09-while.js
@@ -9,3 +9,11 @@ while (year <= 2051){
     console.log("Estamos en el año: " + year);
     year++;
 }
+
+//Do While
+
+var years = 30;
+do {
+    alert("Solo cuando sea diferente a 20");
+    years--;
+} while (years > 25)
