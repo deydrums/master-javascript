824d58a907b12a727db379dbd96c151743288ac8
deydrums <dagarcia100@gmail.com>
2021-05-25 09:26:13 -0600

    Parametros opcionales

--- hola-mundo/js/19-funciones.js
+++ hola-mundo/js/19-funciones.js
@@ -4,19 +4,33 @@
 //Una funcion es una agrupacion reutilizable de un conjunto de instrucciones
 
 //Defino la funcion
-function calculadora(numero1 , numero2) {
+function calculadora(numero1 , numero2, mostrar = false) {
     //Conjunto de instrucciones
-    console.log("Suma: " + (numero1+numero2));
-    console.log("Resta: " + (numero1-numero2));
-    console.log("Multiplicacion: " + (numero1*numero2));
-    console.log("Division: " + (numero1/numero2));
-    console.log("*******************************");
+    if(mostrar == false) {
+        console.log("Suma: " + (numero1+numero2));
+        console.log("Resta: " + (numero1-numero2));
+        console.log("Multiplicacion: " + (numero1*numero2));
+        console.log("Division: " + (numero1/numero2));
+        console.log("*******************************");
+    }else{
+        document.write("Suma: " + (numero1+numero2) + "<br>");
+        document.write("Resta: " + (numero1-numero2) + "<br>");
+        document.write("Multiplicacion: " + (numero1*numero2) + "<br>");
+        document.write("Division: " + (numero1/numero2) + "<br>");
+        document.write("*******************************" + "<br>");       
+    }
+
     //return "Hola soy la calculadora";
 }
 
 //Llamar a la funcion
+calculadora(1,4,true);
+
+/*
 for (let i = 1; i <= 10; i++) {
     console.log(i);
     calculadora(i,8);
     
 }
+*/
+
