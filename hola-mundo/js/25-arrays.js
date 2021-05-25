5de184ffd0dd769bf2a36e7b7f2be57595b1761e
deydrums <dagarcia100@gmail.com>
2021-05-25 11:09:43 -0600

    Array Avanzados

--- hola-mundo/js/25-arrays.js
+++ hola-mundo/js/25-arrays.js
@@ -7,6 +7,20 @@ var nombres = ["David Garcia", "Juan Lopez", "Marco Gonzales", "Jose Rodriguez",
 
 var lenguajes = new Array("PHP" , "JS", "Go", "Java");
 
-console.log(nombres[0]);
+console.log(nombres.length);
 console.log(lenguajes);
 
+var elemento = parseInt(prompt("Que elemento del array quieres?",0));
+
+if (elemento >= nombres.length) {
+    alert("Introduce el numero correcto (menor que "+nombres.length+")")
+}else{
+    alert("El usuario seleccionado es: "+ nombres[elemento]);
+}
+
+document.write("<h1>Lenguajes de programacion <br></h1>");
+document.write("<ul>");
+for (let i = 0; i < lenguajes.length; i++) {
+    document.write("<li>" + lenguajes[i]+"</li>");
+}
+document.write("</ul>");
