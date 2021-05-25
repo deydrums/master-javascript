b7cc927afd61e7432eedb5619e4c5c751dd47f7a
deydrums <dagarcia100@gmail.com>
2021-05-24 22:30:01 -0600

    Ejercicio5, divisores de un numero

---
+++ hola-mundo/js/15-ejercicio5.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/** Muestre todos los numeros divisores de un numero intruduce en prompt */
+
+var numero = parseInt(prompt("Ingresa un numero",1));
+
+for(var i = 1; i <= numero; i++) {
+
+    if(numero % i == 0){
+        console.log("Divisor: "+i);
+    }
+}
