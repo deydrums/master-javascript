70d98dca2156b411e326cfd99bb18183bb9dcc62
deydrums <dagarcia100@gmail.com>
2021-05-25 11:00:55 -0600

    Arrays basicos

---
+++ hola-mundo/js/25-arrays.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Arrays, arreglos, matrices
+
+var nombre = "David Garcia";
+var nombres = ["David Garcia", "Juan Lopez", "Marco Gonzales", "Jose Rodriguez", 52, true];
+
+var lenguajes = new Array("PHP" , "JS", "Go", "Java");
+
+console.log(nombres[0]);
+console.log(lenguajes);
+
