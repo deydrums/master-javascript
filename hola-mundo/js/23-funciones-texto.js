7856bc80652475f30cdc35ddf62f2a43ecbbd022
deydrums <dagarcia100@gmail.com>
2021-05-25 10:44:29 -0600

    Metodos de busqueda

--- hola-mundo/js/23-funciones-texto.js
+++ hola-mundo/js/23-funciones-texto.js
@@ -2,9 +2,18 @@
 
 //Tranformacion de textos
 var numero = 444;
-var texto1 = "Bienvenido al curso de JavaScript";
+var texto1 = "Bienvenido al curso de curso JavaScript";
 var texto2 = "es muy buen curso de JavaScript";
 
+var busqueda = texto1.match(/curso/g);
+var busqueda = texto1.substr(14,5);
+var busqueda = texto1.charAt(29);
+var busqueda = texto1.startsWith("Bi");
+var busqueda = texto1.includes("JavaScript");
+console.log(busqueda);
+
+
+/*
 var dato = numero.toString();
     dato = texto1.toUpperCase();    //Mayusculas    
     dato = texto1.toLowerCase();    //Minusculas
@@ -20,3 +29,4 @@ console.log(nombre.length);
 var textoTotal = texto1 + " " + texto2;
 var textoTotal = texto1.concat(" " + texto2);
 console.log(textoTotal);
+*/
