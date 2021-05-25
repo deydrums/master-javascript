c39f10762fed3f9fb07956fc411643964e0a52f1
deydrums <dagarcia100@gmail.com>
2021-05-25 16:20:36 -0600

    Seleccionar y modificar elementos de la pagina

--- hola-mundo/js/28-dom.js
+++ hola-mundo/js/28-dom.js
@@ -6,6 +6,7 @@ function cambiaColor(color) {
     caja.style.background = color;
 }
 
+//CONSEGUIR ELEMENTOS CON UN ID EN CONCRETO
 
 //var caja = document.getElementById("micaja");
 
@@ -18,4 +19,30 @@ caja.style.color = "white";
 caja.className = "hola hola2";
 
 console.log(caja);
+
+//CONSEGUIR ELEMENTOS POR SU ETIQUETA
+var todosLosDivs = document.getElementsByTagName('div');
+var seccion = document.querySelector("#miseccion");
+var hr = document.createElement("hr");
+
+
+
+//todosLosDivs.forEach((valor, indice) =>{
+var valor;
+for( valor in todosLosDivs){
+    
+    if(typeof todosLosDivs[valor].textContent == 'string'){
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(todosLosDivs[valor].textContent);
+        parrafo.append(texto);
+        seccion.append(parrafo);
+    }
+
+}
+seccion.append(hr);
+//});    
+
+//console.log(contenidoEnTexto);
+
+//CONSEGUIR ELEMENTOS POR SU CLASE CSS
 
