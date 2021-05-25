b52fe620bf5bf21d14b4699238ac43163fcfc079
deydrums <dagarcia100@gmail.com>
2021-05-25 09:49:25 -0600

    Funciones flecha

--- hola-mundo/js/21-funciones-anonimas.js
+++ hola-mundo/js/21-funciones-anonimas.js
@@ -11,10 +11,10 @@ function sumame(numero1 , numero2 , sumaYmuestra, sumaPorDos){
     return sumar;
 }
 
-sumame(5,7,function(dato){
+sumame(5,7,dato=>{
     console.log("La suma es: " + dato);
 },
-function(dato){
+dato=>{
     console.log("La suma por dos es: " + (dato*2));
 });
 
