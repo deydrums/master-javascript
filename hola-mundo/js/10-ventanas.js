e357aa106ee07a180b0dac1dbed42c62106339c5
deydrums <dagarcia100@gmail.com>
2021-05-24 21:41:48 -0600

    Alertas y ventanas

---
+++ hola-mundo/js/10-ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Alerta
+alert("Hola que tal");
+
+//Confirmacion
+var mi_resultado = confirm("¿Estas seguro de querer continuar?");
+console.log(mi_resultado);
+
+// Ingreso datos
+var mi_resultado = prompt("¿Que edad tienes?",18);
+console.log(mi_resultado);
