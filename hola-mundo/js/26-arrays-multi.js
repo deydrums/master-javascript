bf2795c158520ec697a40cc399c3f53a8e9cf29e
deydrums <dagarcia100@gmail.com>
2021-05-25 11:18:53 -0600

    Arrays multidimensionales

---
+++ hola-mundo/js/26-arrays-multi.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Arrays multidimensionales
+
+var categorias = ['Accion' , 'Terror', 'Comedia'];
+var peliculas = ['La verdad duele' , 'La vida es bella', 'Ghost Rider'];
+
+var cine = [categorias,peliculas];
+
+console.log(cine[0][1]);
