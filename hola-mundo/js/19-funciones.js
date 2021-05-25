610992d3fff9e7fc552607f0e744db90758c5cc6
deydrums <dagarcia100@gmail.com>
2021-05-25 09:13:32 -0600

    Crear funciones

---
+++ hola-mundo/js/19-funciones.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//FUNCIONES
+//Una funcion es una agrupacion reutilizable de un conjunto de instrucciones
+
+//Defino la funcion
+
+function calculadora() {
+    //Conjunto de instrucciones
+    console.log("Hola, soy la calculadora");
+    //return "Hola soy la calculadora";
+}
+
+//Llamar a la funcion
+calculadora();
+calculadora();
+calculadora();
+calculadora();
