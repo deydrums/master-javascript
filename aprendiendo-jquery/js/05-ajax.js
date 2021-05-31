363b030651629000d0cb3d7e4a7e85a2f459de45
deydrums <dagarcia100@gmail.com>
2021-05-31 14:29:01 -0600

    Metodo get - ajax

--- aprendiendo-jquery/js/05-ajax.js
+++ aprendiendo-jquery/js/05-ajax.js
@@ -2,5 +2,13 @@
 
 $(document).ready(function(){
     //load
-    $("#datos").load("https://reqres.in/");
+    //$("#datos").load("https://reqres.in/");
+
+    //Get y Post
+    $.get("https://reqres.in/api/users",{page:2},function(response){
+        response.data.forEach((element,index)=>{
+            $("#datos").append("<p>"+element.first_name+"  "+ element.last_name +"</p>");
+        });
+    });
+
 });
