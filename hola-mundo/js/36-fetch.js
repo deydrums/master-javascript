130cd3035e43be086b1d3d0b24cffb548051ee37
deydrums <dagarcia100@gmail.com>
2021-05-31 06:53:17 -0600

    Como crear promesas

--- hola-mundo/js/36-fetch.js
+++ hola-mundo/js/36-fetch.js
@@ -3,24 +3,50 @@
 //Fetch (ajax) y peticiones a servicios / apis rest
 var div_usuarios = document.querySelector('#usuarios');
 var div_Janet = document.querySelector('#janet');
+var div_profesor = document.querySelector('#profesor');
 
     getUsuarios()
     .then(data => data.json())
     .then(users => {
         listadoUsuarios(users.data);
+        return getInfo();
+    })
+    .then(data =>{
+        div_profesor.innerHTML = data;
         return getJanet();
     })
     .then(data => data.json())
     .then(user => {
         mostrarJanet(user.data);
-
+        
     });
+
 
 function getUsuarios(){
     return fetch('https://reqres.in/api/users?page=2');
 }
 function getJanet(){
     return fetch('https://reqres.in/api/users/2');
+}
+
+//Crear promesa
+
+function getInfo(){
+    var profesor = {
+        nombre: 'David',
+        apellidos: 'Garcia',
+        url: 'https://deydrums.com'
+    };
+
+    return new Promise((resolve,reject) => {
+        var profesor_string = "";
+        setTimeout(() => {
+            profesor_string = JSON.stringify(profesor);
+            if (typeof profesor_string != 'string' || profesor_string == '') return reject('error');
+            return resolve(profesor_string);
+        }, 3000);
+          
+    });
 }
 
 
