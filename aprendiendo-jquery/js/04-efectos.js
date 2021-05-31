923a3af1df38aa758bc5c9c5478157369601ab17
deydrums <dagarcia100@gmail.com>
2021-05-31 13:58:47 -0600

    Animaciones personalizadas

--- aprendiendo-jquery/js/04-efectos.js
+++ aprendiendo-jquery/js/04-efectos.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var caja = $('#caja');
     console.log("jQuery y la web cargados");
 
     $('#mostrar').hide();
@@ -6,16 +7,41 @@ $(document).ready(function(){
     $('#mostrar').click(function(){
         $(this).hide();
         $('#ocultar').show();
-        $('#caja').slideDown('slow');
+        caja.slideDown('slow');
     });
 
     $('#ocultar').click(function(){
         $(this).hide();
         $('#mostrar').show();
-        $('#caja').slideUp('slow');
+        caja.slideUp('slow');
     });
 
     $('#todoenuno').click(function(){
-        $('#caja').slideToggle('fast');
+        caja.slideToggle('fast');
+    });
+
+    $('#animar').click(function(){
+        caja.animate({
+                    marginLeft:'500px',
+                    fontSize:'40px',
+                    height:'110px'
+                    },'slow')
+            .animate({
+                borderRadius: '900px',
+                marginTop: '80px'
+            },'slow')
+            .animate({
+                borderRadius: '0px',
+                marginLeft: '0px'
+            },'slow')
+            .animate({
+                borderRadius: '100px',
+                marginTop: '0px'
+            },'slow')
+            .animate({
+                marginLeft:'500px',
+                fontSize:'40px',
+                height:'110px'
+                },'slow');
     });
 });
