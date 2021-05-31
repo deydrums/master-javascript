d26cad3bd145d0a18050414b9c38f7be008ebc7f
deydrums <dagarcia100@gmail.com>
2021-05-31 10:22:25 -0600

    MouseOver y MouseOut

---
+++ aprendiendo-jquery/js/02-eventos.js
@@ -0,0 +1,18 @@
+'use strict'
+
+$(document).ready(function() {
+    //MouseOver y MouseOut
+    var caja = $("#caja");
+
+    caja.mouseover(function(){
+        $(this).css("background","red");
+    });
+
+    caja.mouseout(function(){
+        $(this).css("background","green");
+    });
+
+
+
+
+});
