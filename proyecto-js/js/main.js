e8ff2391ffa37d249bde598d05248d4193c49abe
deydrums <dagarcia100@gmail.com>
2021-05-31 20:44:12 -0600

    Maquetacion del menu

---
+++ proyecto-js/js/main.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".boton-menu");
+const navMenu = document.querySelector(".menu ul");
+const navLink = document.querySelectorAll(".menu ul li a");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
