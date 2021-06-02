2eacaf8fb7023be1fa51675cbb29e12877000837
deydrums <dagarcia100@gmail.com>
2021-06-01 21:26:42 -0600

    Cambiador de temas del sitio web

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -47,4 +47,18 @@ $(document).ready(function(){
 
       $("#posts").append(post);
     });
+
+    //Selector de tema
+    var theme = $("#theme");
+    $("#to-green").click(function(){
+      theme.attr("href", "css/green.css");
+    });
+
+    $("#to-blue").click(function(){
+      theme.attr("href", "css/blue.css");
+    });
+
+    $("#to-red").click(function(){
+      theme.attr("href", "css/red.css");
+    });
 });
