bdfd833f58eed2a34614e49ed8f30fa712d19214
deydrums <dagarcia100@gmail.com>
2021-06-02 17:07:50 -0600

    Variables y tipos de datos

---
+++ poo-y-typescript/js/03-tipos-de-datos.js
@@ -0,0 +1,12 @@
+//String
+var cadena = "David";
+//number
+var numero = 12;
+//booleano
+var verdadero_falso = true;
+//Any
+var cualquiera = "hola";
+//arrays
+var lenguajes = ["PHP", "JS", "CSS"];
+var years = [2021, 1989, 1999];
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
---
+++ poo-y-typescript/js/03-tipos-de-datos.ts
@@ -0,0 +1,19 @@
+//String
+let cadena: string = "David";
+
+//number
+let numero: number = 12;
+
+//booleano
+let verdadero_falso: boolean = true;
+
+//Any
+let cualquiera: any = "hola";
+
+//arrays
+var lenguajes: Array<string> = ["PHP" , "JS" , "CSS"];
+var years: Array<number> = [2021 , 1989, 1999];
+
+
+console.log(cadena, numero, verdadero_falso,cualquiera, lenguajes, years);
+
