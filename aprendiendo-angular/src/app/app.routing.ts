87d173dcf6995bd8495d8e30c1237d050f88287b
deydrums <dagarcia100@gmail.com>
2021-06-04 12:58:26 -0600

    Servicios y HttpClient

--- aprendiendo-angular/src/app/app.routing.ts
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 import{ZapatillasComponent} from './zapatillas/zapatillas.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { CursosComponent } from './cursos/cursos.component';
-
+import { ExternoComponent } from './externo/externo.component';
 //Array de rutas
 const appRoutes: Routes = [
     {path: '',component: HomeComponent},
@@ -17,6 +17,7 @@ const appRoutes: Routes = [
     {path: 'videojuego',component: VideojuegoComponent},
     {path: 'cursos',component: CursosComponent},
     {path: 'cursos/:nombre/:fallowers',component: CursosComponent},
+    {path: 'externo',component: ExternoComponent},
     {path: '**' ,component: HomeComponent}
 ];
 
