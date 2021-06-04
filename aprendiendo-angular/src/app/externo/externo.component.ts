ecb56b9454041a1502b93d07f2f4824f54f44c2b
deydrums <dagarcia100@gmail.com>
2021-06-04 13:00:53 -0600

    Efecto de carga

--- aprendiendo-angular/src/app/externo/externo.component.ts
+++ aprendiendo-angular/src/app/externo/externo.component.ts
@@ -22,6 +22,7 @@ export class ExternoComponent implements OnInit {
   }
 
   cargaUsuario(){
+    this.user = false;
     this._peticionesService.getUser(this.userId).subscribe(
       result => {
         this.user = result.data;
