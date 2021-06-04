a7161edf6f78f3203ad9260704a21f88d32938b7
deydrums <dagarcia100@gmail.com>
2021-06-04 09:10:03 -0600

    Eveto Click

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -45,4 +45,9 @@ export class ZapatillasComponent implements OnInit {
     addMarca(){
         this.marcas.push(this.mi_marca);
     }
+
+    borrarMarca(index:number){
+        //delete this.marcas[index];
+        this.marcas.splice(index, 1);
+    }
 }
