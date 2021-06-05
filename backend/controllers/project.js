44130f19c4fc21afe79a11837429b25e57d5e461
deydrums <dagarcia100@gmail.com>
2021-06-05 09:00:57 -0600

    Listar proyectos del portafolio

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -31,6 +31,24 @@ var controller = {
             if(!projectStored) return res.status(404).send({message:"No se ha podido guardar el proyecto"});
             return res.status(200).send({project: projectStored});
         });
+    },
+
+
+    getProject: function(req, res){
+        var projectId = req.params.id;
+
+        if(projectId == null) return res.status(404).send({message: 'El proyecto no existe'});
+        
+        Project.findById(projectId, (err, project) =>{
+            if(err) return res.status(500).send({message: 'Error al devolver los datos'});
+            if(!project) return res.status(404).send({message: 'El proyecto no existe'});
+
+            return res.status(200).send({
+                project
+            });
+        });
+
+
     }
 };
 
