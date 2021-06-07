f8149527faafdc78b74e0b74c40d3ac191b78c23
deydrums <dagarcia100@gmail.com>
2021-06-07 15:58:26 -0600

    Actualizar imagen

---
+++ proyecto-angular/src/app/components/edit/edit.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+import { UploadService } from '../../services/upload.service';
+import { Global } from '../../services/global';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: '../create/create.component.html',
+  styleUrls: ['./edit.component.css'],
+  providers: [ProjectService, UploadService]
+})
+export class EditComponent implements OnInit {
+  public title: string;
+  public project: Project;
+  public save_project: any;
+  public status: string;
+  public filesToUpload: Array<File>;
+
+  constructor(
+    private _projectService: ProjectService,
+    private _uploadService: UploadService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) { 
+    this.title = "Editar proyecto";
+    this.project = new Project('','','','',2021,'','');
+    this.status = '';
+    this.filesToUpload = [];
+  }
+
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      let id = params.id;
+      this.getProject(id);
+    });
+  }
+
+  getProject(id:any){
+    this._projectService.getProject(id).subscribe(
+      response => {
+        this.project = response.project;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  onSubmit(form:any){
+    this._projectService.updateProject(this.project).subscribe(
+      response => {
+        if(response.project){
+          
+          //Subir imagen
+          if(this.filesToUpload.length >= 1){
+            this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id,[],this.filesToUpload,'image').then((result:any) => {
+              console.log(result);
+              this.save_project = result.project;
+              this.status = 'success';
+            });
+          }
+         
+          
+        }else{
+          this.status='failed';
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  fileChangeEvent(fileInput:any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+  }
+
+}
