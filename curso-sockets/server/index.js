82d0ed3b11bdea82571cb203a19c122b075771b1
deydrums <dagarcia100@gmail.com>
2021-06-07 21:43:42 -0600

    Mensajes en el chat

--- curso-sockets/server/index.js
+++ curso-sockets/server/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server,{
+    cors: { // Permite el acceso de orígenes mixtos (CORS)
+        origin: '*'
+    }
+});
 
 app.use(express.static('client'));
 
@@ -10,8 +14,16 @@ app.get('/hola-mundo',function(req, res){
     res.status(200).send('Hola mundo desde una ruta');
 });
 
+var messages = [{
+    id: 1,
+    texto: 'Bienvenido al chat privado de socket.io y NodeJS',
+    nickname: 'Bot - deydrums'
+}];
+
+
 io.on('connection', function(socket){
-    console.log("El cliente con IP: " + socket.handshake.address+ " se ha conectado.");
+    console.log("El cliente con IP: " + socket.handshake.address + " se ha conectado.");
+    socket.emit('messages',messages);
 });
 
 server.listen(6677,function(){
