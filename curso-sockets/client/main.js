f4564fc4bf4ae947eba286385037621cabad764b
deydrums <dagarcia100@gmail.com>
2021-06-07 22:18:02 -0600

    Enviar y recibir mensajes

--- curso-sockets/client/main.js
+++ curso-sockets/client/main.js
@@ -13,6 +13,18 @@ function render (data){
         </div>
         `);
     }).join(' ');
+    var div_msgs = document.getElementById('messages')
+    div_msgs.innerHTML = html;
+    div_msgs.scrollTop = div_msgs.scrollHeight;
+}
 
-    document.getElementById('messages').innerHTML = html;
+function addMessage(e){
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
+
+    document.getElementById('nickname').style.display = 'none';
+    socket.emit('add-message', message);
+    return false;
 }
--- curso-sockets/server/index.js
+++ curso-sockets/server/index.js
@@ -24,6 +24,10 @@ var messages = [{
 io.on('connection', function(socket){
     console.log("El cliente con IP: " + socket.handshake.address + " se ha conectado.");
     socket.emit('messages',messages);
+    socket.on('add-message', function(data){
+        messages.push(data);
+        io.sockets.emit('messages',messages);
+    });
 });
 
 server.listen(6677,function(){
